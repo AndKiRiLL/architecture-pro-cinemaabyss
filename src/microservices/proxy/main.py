bd969a662997c11e044f1e98865a6a0558773faa
Kirill <kirill21012023@gmail.com>
2026-05-31 16:00:37 +0300

    Добавил для CI ветку cinema

--- src/microservices/proxy/main.py
+++ src/microservices/proxy/main.py
@@ -11,6 +11,7 @@ logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
+
 # --- Конфигурация из переменных окружения ---
 MONOLITH_URL = os.getenv("MONOLITH_URL", "http://monolith:8080")
 MOVIES_SERVICE_URL = os.getenv("MOVIES_SERVICE_URL", "http://movies-service:8081")
